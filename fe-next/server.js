05a567a602c8e0b58e4d6e674782339d3ddbf2ac
Ohad Fisher <ohad@appdome.com>
2025-11-23 10:59:30 +0200

    feat: remove static index.html and implement dynamic locale-based root path redirection in server.js

--- fe-next/server.js
+++ fe-next/server.js
@@ -161,12 +161,35 @@ app.prepare().then(() => {
   // Next.js handler for all other routes
   server.use(async (req, res) => {
     try {
-      const parsedUrl = new URL(req.url, `http://${req.headers.host}`);
-      const { pathname } = parsedUrl;
+      const parsedUrl = require('url').parse(req.url, true);
+      const { pathname, query } = parsedUrl;
       
+      console.log(`[SERVER] Request: ${req.method} ${pathname}`);
+
+      // Manual redirect for root path (since middleware might be skipped in custom server)
+      if (pathname === '/') {
+        const acceptLanguage = req.headers['accept-language'];
+        let locale = 'he'; // Default
+        
+        if (acceptLanguage) {
+          const browserLang = acceptLanguage.split(',')[0].split('-')[0];
+          if (['en', 'he'].includes(browserLang)) {
+            locale = browserLang;
+          }
+        }
+        
+        console.log(`[SERVER] Redirecting / to /${locale}`);
+        res.writeHead(307, { Location: `/${locale}` });
+        res.end();
+        return;
+      }
+
       if (pathname === '/sitemap.xml' || pathname === '/robots.txt') {
         console.log(`[SERVER] Serving static file: ${pathname}`);
       }
+      
+      // Set x-powered-by to Next.js to reassure user
+      res.setHeader('X-Powered-By', 'Next.js');
 
       await handle(req, res, parsedUrl);
     } catch (err) {
