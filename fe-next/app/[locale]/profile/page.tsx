255bcb18e067bbed755fa01fb5605d2f161b35e7
Ohad Fisher <ohad@appdome.com>
2026-01-02 17:35:19 +0200

    feat: Enhance profile page styling with mobile tab visibility and improved background effects

--- fe-next/app/[locale]/profile/page.tsx
+++ fe-next/app/[locale]/profile/page.tsx
@@ -1126,7 +1126,7 @@ export default function ProfilePage(): React.ReactNode {
         </div>
 
         {/* Mobile Tab Bar */}
-        <div className="fixed bottom-0 left-0 right-0 z-50">
+        <div className="fixed bottom-0 left-0 right-0 z-50 lg:hidden">
           <MobileTabBar
             tabs={mobileTabs}
             activeTab={mobileActiveTab}
