ab9f446ef3795e511d707fe010a7d794271513cb
Ohad Fisher <ohad@appdome.com>
2026-01-08 17:39:39 +0200

    refactor: adjust padding in InGameScreen to prevent scroll issues

--- fe-next/components/game/InGameScreen.tsx
+++ fe-next/components/game/InGameScreen.tsx
@@ -1045,7 +1045,7 @@ const InGameScreen = memo<InGameScreenProps>(({
 
           {/* Grid - Direct connection to timer row */}
           {/* Removed extra padding to prevent scroll - safe area handled by parent */}
-          <div className="flex-1 flex items-center justify-center min-h-0 overflow-hidden -mt-4 md:-mt-6 lg:-mt-8">
+          <div className="flex-1 flex items-start justify-center min-h-0 overflow-hidden">
             <GridComponent
               key={isPlaying ? 'playing-grid' : 'spectating-grid'}
               grid={letterGrid}
