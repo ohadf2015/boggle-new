c78324e25b0bf30cc3db7155becda9d55532dc6b
Ohad Fisher <ohad@appdome.com>
2026-01-26 10:22:30 +0200

    feat(25-05): add native components barrel export
    - Export OfflineFallback, NativeAppProvider, NetworkStatusHandler
    - Named exports for clean imports
    - Enables: import { NativeAppProvider } from '@/components/native'

---
+++ fe-next/components/native/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Native components barrel export
+ *
+ * Provides clean imports for native integration components.
+ */
+
+export { OfflineFallback } from './OfflineFallback';
+export { NativeAppProvider } from './NativeAppProvider';
+export { NetworkStatusHandler } from './NetworkStatusHandler';
