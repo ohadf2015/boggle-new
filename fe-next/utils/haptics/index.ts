8d479fa5d293d119ecbcbbf587e28b4b1d4e703b
Ohad Fisher <ohad@appdome.com>
2026-01-26 10:40:20 +0200

    feat(25-06): create barrel export for clean imports
    - Exports all public APIs from haptics module
    - Type-only exports for TypeScript interfaces
    - Enables clean imports: import { haptics, HapticPattern } from '@/utils/haptics'

---
+++ fe-next/utils/haptics/index.ts
@@ -0,0 +1,9 @@
+export { HapticsManager, haptics } from './HapticsManager';
+export { WebHaptics } from './webHaptics';
+export { NativeHaptics } from './nativeHaptics';
+export {
+  HapticPattern,
+  HapticIntensity,
+  type CustomHapticPattern,
+  type IHapticsImplementation,
+} from './types';
