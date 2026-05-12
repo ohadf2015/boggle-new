7a264024775090957dafd1f2c5fab756c677bc66
Ohad Fisher <ohad@appdome.com>
2026-05-12 13:03:50 +0300

    feat(blast-v2): Spanish locale w/ accent-fold + ñ (Plan 1 Task 8)

--- fe-next/lib/blast/v2/locale-config.ts
+++ fe-next/lib/blast/v2/locale-config.ts
@@ -3,6 +3,7 @@ import { EN_CONFIG } from './locales/en';
 import { HE_CONFIG } from './locales/he';
 import { SV_CONFIG } from './locales/sv';
 import { JA_CONFIG } from './locales/ja';
+import { ES_CONFIG } from './locales/es';
 
 export type ThemeDef = {
   key: ThemeKey;
@@ -29,4 +30,5 @@ export const LOCALE_CONFIGS: Record<Locale, LocaleConfig> = {
   he: HE_CONFIG,
   sv: SV_CONFIG,
   ja: JA_CONFIG,
-} as Record<Locale, LocaleConfig>;
+  es: ES_CONFIG,
+};
